6839ae9379e01b62dacaee63c18e8eed859ec29d
albihasani94 <albihasani94@gmail.com>
2020-01-22 17:20:16 +0100

    Touch one thing or two on jbang

--- src/play.with.records/play/with/records/RecordRunner.java
+++ src/play.with.records/play/with/records/RecordRunner.java
@@ -1,60 +1,8 @@
-//usr/bin/env jbang "$0" "$@" ; exit $?
-// JAVAC_OPTIONS --enable-preview -source 14 -Xlint:preview
-//JAVA_OPTIONS --enable-preview
 package play.with.records;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.Objects;
 
-class Person {
-
-    private String name;
-    private int age;
-
-    public Person(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return age == person.age &&
-                Objects.equals(name, person.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age);
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-}
 public class RecordRunner {
 
     public static void main(String[] args) {
@@ -95,21 +43,22 @@ public class RecordRunner {
 
     static int counter = 0;
 
-    record PersonYears (Person person, int years) { }
+    record PersonYears(Person person,int years) {
+    }
 
-    static int yearsTo100 (int age) {
+    static int yearsTo100(int age) {
         counter++;
         return 100 - age;
     }
 
-    record Person (String name, int age) {
+    record Person(String name,int age) {
         public Person {
             if (age > 135) {
                 throw new IllegalArgumentException("Sorry, age (%d) is not supported".formatted(age));
             }
         }
 
-        boolean isWise () {
+        boolean isWise() {
             return age > 80;
         }
     }
